f4af300b23a45de76a5dc09518eb6f22ba699d2c
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-09-19 11:05:04 -0300

    Criação das entidades Usuario e Grupo e seus relacionamentos

---
+++ algafood-api/src/main/java/com/rodrigo/api_delivery/model/Grupo.java
@@ -0,0 +1,30 @@
+package com.rodrigo.api_delivery.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+public class Grupo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+
+	private String nome;
+	
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"),inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes = new ArrayList<>();
+
+}
---
+++ algafood-api/src/main/java/com/rodrigo/api_delivery/model/Usuario.java
@@ -0,0 +1,44 @@
+package com.rodrigo.api_delivery.model;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String senha;
+	
+	@CreationTimestamp
+	@Column(columnDefinition = "datetime")
+	private OffsetDateTime dataCadastro;
+	
+	@ManyToMany
+    @JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos;
+
+}
